1e55dd906d468b148f48fea1b5e5153806dda5ce
JuanMa Garrido <juanma.garrido@gmail.com>
2020-06-29 15:51:25 +0200

    more simple example

--- api/index.js
+++ api/index.js
@@ -4,21 +4,3 @@ module.exports = (req, res) => {
     city: 'Murcia'
   })
 }
-
-/*
-const { chain } = require('@amaurymartiny/now-middleware');
-const { NowRequest, NowResponse } = require('@vercel/node');
-
-// Import a couple of Express middlewares
-const cors = require('cors'); // Enable cross-origin resource sharing (CORS) with various options
-const morgan = require('morgan'); // HTTP request logger
-const server = require('../build/server.js'); // HTTP request logger
-
-async function handler(req, res) {
-  // This is your normal ZEIT Now function.
-  res.send('Everything OK.');
-}
-
-// Chain some middlewares before calling the ZEIT Now serverless function
-module.exports = chain(cors(), morgan('common'), server)(handler);
-*/
